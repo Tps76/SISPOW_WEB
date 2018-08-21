3008b3d8f1fa735b91fe16f989e22aa8dc993b99
Santiago <zmtfcaz@gmail.com>
2018-08-21 17:42:41 -0500

    cambio 1

--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -1,12 +1,29 @@
 <?php
+
+ 
+
 class conexion{
 public static function credencial(){
 	$hostname="localhost";
         $database="SISPOW";
         $username="root";
-        $password="k";
+        $password="";
         $conex=mysqli_connect($hostname,$username,$password,$database);
+        
+        if ($conex) {
+                echo "conexion valida";
+        }else{
+                echo "conexion invalida";
+        }
+        
         return $conex;
 }
 }
+
+$conexion = new connection; 
+
+$conexion -> credencial();
+
+
+
 ?>
