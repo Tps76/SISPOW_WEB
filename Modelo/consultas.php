e5f92eebc39c766d2d2f5d62e2d7f6819d0b5c37
julian <xpry@gmail.com>
2018-08-22 20:05:05 -0500

    actualizacion con todo comentado con ejemplos

--- Modelo/consultas.php
+++ Modelo/consultas.php
@@ -1,5 +1,20 @@
 <?php
-require_once("conexion.php");
-class consultas{
+require_once("conexion.php");//requiere conexion.php
+class consultas{//clase consultas
+    public function ejemplo($consulta){//funcion ejemplo que recibe una consulta
+    $conexion=conexion::credencial();//llamamos la clase conexion y ejecutamos la funcion credencial
+    $resultado=mysql_query($conexion,$consulta);//ejecutamos un query con la conexion recibida y la consulta dada al inicio de la funcion
+    if($resultado){//si se ejecuta correctamente la consulta
+        //if se utiliza con un array o solo una cadena de datos
+        if($datos_if=mysqli_fetch_array($resultado)){//si recupera datos de la consulta, guardarlos en la variable datos_if
+            return $datos_if;//retone datos_if
+        }
+        //while se utiliza con muchos arrays o varias cadenas de datos
+        while($datos_while=mysqli_fetch_array($resultado)){//mientras recupera datos de la consulta, guardarlos en la variable datos_while
+            return $datos_while;//retorne datos_while
+        }
+    }
+    }
 }
+//este archivo contiene consultas generalizadas donde le enviaremos un string con la consulta y nos devolvera el resultado.
 ?>
