a833e98e33c6454a3339ab82cfdb79bd0cfc9e42
julian <xpry@gmail.com>
2018-08-22 20:39:37 -0500

    actualizacion con todo comentado con ejemplos

--- Modelo/acciones.php
+++ Modelo/acciones.php
@@ -1,9 +1,11 @@
 <?php
-include("consultas.php");
-class acciones{
-
+require_once("consultas.php");//requiere consultas.php
+class acciones{//clase acciones
+    public function prueba(){// funcion prueba
+    $consulta="sentencia sql";//sentencia sql que sera enviada a la consulta generalizada
+    $resultado=consultas::ejemplo($consulta);//variable que guardara el retorno de la consulta generalizada
+    }
 }
-$acciones = new acciones();
-//$acciones->function;
-$hola=0;
+$acciones = new acciones();//llamada a clase acciones instanciandola
+$acciones->prueba;//llamada a la funcion prueba
 ?>
--- Modelo/conexion.php
+++ Modelo/conexion.php
@@ -1,23 +1,20 @@
 <?php
-class conexion{
-	
-public static function credencial(){
-	$hostname="localhost";
-        $database="id6313732_sispow";
-        $username="id6313732_oscar";
-        $password="12345";
-        $conex=mysqli_connect($hostname,$username,$password,$database);
-        
-        if ($conex) {
+class conexion{ //clase conexion
+public static function credencial(){ //funcion integrada en la clase conexion
+        $hostname="localhost"; //nombre del servidor
+        $database="id6313732_sispow"; //nombre de la base de datos
+        $username="id6313732_oscar"; //usuario del servidor
+        $password="12345"; //contraseña del servidor
+        $conex=mysqli_connect($hostname,$username,$password,$database);//variable donde se guarda la conexion para ser reutilizada
+        if ($conex) {//si conexion es realizada realizar echo y retornar conexion
                 echo "conexion valida";
-        }else{
+                return $conex;
+        }else{//si conexion no es realizada realizar echo
                 echo "conexion invalida";
         }
-        
-        return $conex;
 }
 }
-$conexion = new connection; 
-$conexion -> credencial();
-
+$conexion = new connection;//llamada a clase y se instancia 
+$conexion -> credencial();//llamada a funcion contenida en conexion
+//esta llamada no es necesaria si ya se tiene la conexion. Se procede con la consulta.
 ?>
