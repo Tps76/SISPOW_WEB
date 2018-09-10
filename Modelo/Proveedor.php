7bc0ad908c101046b033dad0fbb0d723762c42df
Dany <dany.mrtinez@gmail.com>
2018-09-09 20:12:05 -0500

    update

--- Modelo/Proveedor.php
+++ Modelo/Proveedor.php
@@ -4,10 +4,6 @@ require_once '../Modelo/conexion.php';
 class Proveedor
 {
 
-    public function regProv()
-    {
-         
-    }
     public function getAllProv()
     {
         $consulta = "SELECT * FROM proveedor";
@@ -22,7 +18,7 @@ class Proveedor
     public function getProv($id, $nombre)
     {
         $consulta = "SELECT * FROM proveedres WHERE idproveedor LIKE '$id' OR nombre_proveedor LIKE ".$nombre;
-        $consultas = consultas::buscar($consulta);
+        $consultas = consultas::seleccionar_datos($consulta);
         if ($consultas) {
             return $consultas->fetch_assoc();
         }else{
@@ -41,6 +37,17 @@ class Proveedor
         $email = $datos['email'];
         $contacto = $datos['contacto'];
         $consulta = "INSERT INTO proveedor(idproveedor, razon_social, nombre_proveedor, apellido_proveedor, direccion_proveedor, email_proveedor, telefono_proveedor) VALUES('$nit','$razon','$nombre','$apellido','$direccion','$email','$contacto')";
-        
+        $consultas = consultas::insertar($consulta);
+        return $consultas;
+    }
+    public function loadProv()
+    {
+        $consulta = "SELECT idproveedor, nombre_proveedor, apellido_proveedor,razon_social FROM proveedor";
+        $consultas = consultas::seleccionar_datos($consulta);
+        if ($consultas) {
+            return array($consultas->fetch_assoc());
+        }else{
+            return "error";
+        }
     }
 }
