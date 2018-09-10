3fd7ce2f485c60abf2145020cc80ac10650da05f
Dany <dany.mrtinez@gmail.com>
2018-09-09 20:13:24 -0500

    update parte productos, categorias

--- Controlador/adminController.php
+++ Controlador/adminController.php
@@ -2,6 +2,8 @@
 require_once '../Modelo/consultas.php';
 require_once '../Modelo/Ruta.php';
 require_once '../Modelo/Proveedor.php';
+require_once '../Modelo/Producto.php';
+require_once '../Modelo/Categoria.php';
 
 
 class adminController
@@ -17,7 +19,7 @@ class adminController
         if ($respuesta != "error") {
             // $contador = count($respuesta);
             foreach ($respuesta as $result) { 
-                echo '  <tr>    
+                echo '<tr>    
                         <td>'.$result['idproveedor'].'</td>
                         <td>'.$result['razon_social'].'</td>
                         <td>'.$result['nombre_proveedor']." ".$result['apellido_proveedor'].'</td>
@@ -86,7 +88,69 @@ class adminController
         }
     }
 
-    public function rutaAdminController(){
+    public function getProdAll()
+    {
+        // IMPORTANTE hay que hacer un inner join para colocar la razon social del proveedor en la tabla de productos.
+        $respusta = Producto::getAllProd();
+        if ($respuesta != "error") {
+            foreach ($respuesta as $result) { 
+                // Colocar los nombres respectivos.
+                echo '<tr>    
+                        <td>'.$result[''].'</td>
+                        <td>'.$result[''].'</td>
+                        <td>'.$result[''].'</td>
+                        <td>'.$result[''].'</td>
+                        <td>'.$result[''].'</td>
+                        <td>'.$result[''].'</td>
+                        <td>
+                        <div class="d-flex justify-content-between">
+                            <a class="btn btn-outline-primary" data-toggle="modal" href="#editar"><i class="material-icons d-flex align-item-center ">edit</i> </a>
+                            <a class="btn btn-outline-danger" data-toggle="modal" href="#eliminar"><i class="material-icons d-flex align-item-center ">delete</i> </a>
+                        </div>
+                        </td>
+                    </tr>';
+            }
+        }
+    }
+
+    public function selectCat()
+    {
+        $respuesta = Categoria::loadCat();
+        if ($respuesta != "error") {
+            foreach ($respuesta as $result) {
+                echo '<option value="'.$result['idcatproducto'].'">'.$result['nombre_categoria'].'</option>';
+            }
+        }
+    }
+
+    public function selectProv()
+    {
+        $respuesta = Proveedor::loadProv();
+        if($respuesta != "error"){
+            foreach ($respuesta as $result) {
+                echo '<option value="'.$result['idproveedor'].'">'.$result['razon_social']." - ".$result['nombre_proveedor']." ".$result['apellido_proveedor'].'</option>';
+            }
+        }
+    }
+
+    public function regProd()
+    {
+        if (isset($_POST['regProdname'])) {
+            // PARA SUBIR IMAGENES AL SERVIDOR
+            if (isset($_FILES['img'])) {
+                $dir = $_SERVER['DOCUMENT_ROOT'].'SISPOW_WEB/Vistas/src/assets/img';
+                $img = $_FILES['img'];
+                $share = uploadFile::upload($dir, $img);
+            }else{
+                $share = null;
+            }
+
+
+        }
+    }
+    
+    public function rutaAdminController()
+    {
         if(isset($_GET['action'])){
             $enlace = $_GET['action'];
             $ruta = Ruta::rutaAdmin($enlace);
